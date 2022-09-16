841cd21bcf95284583cfe8b5747dbbb80381701f
Dwsy <1521986032@qq.com>
2022-09-17 01:39:54 +0800

    ~

--- ddl-service/ddl-service-message/src/main/java/link/dwsy/ddl/messageApplication.java
+++ ddl-service/ddl-service-message/src/main/java/link/dwsy/ddl/messageApplication.java
@@ -10,14 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * @Date 2022/8/16
  */
 @SpringBootApplication(scanBasePackages = {"link.dwsy.ddl"})
-//@EnableDiscoveryClient
 //@EnableDiscoveryClient(autoRegister = false)
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
-//@EntityScan("import link.dwsy.ddl.entity")
-public class templateApplication {
+public class messageApplication {
     public static void main(String[] args) {
-        SpringApplication.run(templateApplication.class, args);
+        SpringApplication.run(messageApplication.class, args);
     }
 
 }
